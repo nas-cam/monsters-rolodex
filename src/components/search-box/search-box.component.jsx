777764b1aa7bc1648ee0e97d587d3e5e5b9502a0
Nasveta Camdzic <nasveta.c@gmail.com>
2021-11-22 11:24:00 +0100

    Finished project

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -3,5 +3,10 @@ import React from "react";
 import "./search-box.styles.css";
 
 export const SearchBox = ({ placeholder, handleChange }) => (
-  <input type="search" placeholder={placeholder} onChange={handleChange} />
+  <input
+    className="search"
+    type="search"
+    placeholder={placeholder}
+    onChange={handleChange}
+  />
 );
